8c04d320e83cb5610e4ec58b4227b594f9d1577a
Rhys Miller <miller5157r@CKSD.WEDNET.EDU>
2016-10-25 10:47:23 -0700

    Searching set up

--- Analysis.java
+++ Analysis.java
@@ -1,11 +1,21 @@
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Set;
 public class Analysis
 {
    public static void main(String[] arg)
    {
-      Book book = new Book("");
-      TreeMap<Integer,String> words = new TreeMap();
-      
+      Book book = new Book("EntireIliad.txt");
+      String[] putThese = {"rage","anger"};
+      Map<Integer,String> words = new TreeMap();
+      for(String i:putThese)
+      {
+         words.putAll(book.searchFor(i));
+      }
+      Set<Integer> keys = words.keySet();
+      for(int i:keys)
+      {
+         System.out.println(i+":"+words.get(i));
+      }
    }
 }
--- Book.java
+++ Book.java
@@ -1,31 +1,38 @@
-import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 import java.util.Map;
+import java.util.TreeMap;
 public class Book
 {
    private String fullText;
    public Book(String address)
    {
-    /*
-   	 try
-     {
-       StringBuilder build = new StringBuilder();
-       Scanner scan = new Scanner(address);
-       while(scan.hasNextLine())
-       {
-         build.append(scan.nextLine()+"\n");
+      try
+      {
+         StringBuilder build = new StringBuilder();
+         Scanner scan = new Scanner(new FileReader(address));
+         while(scan.hasNextLine())
+         {
+            build.append(scan.nextLine().toLowerCase()+"\n");
+         }
          fullText = build.toString();
-       }
-     }
-    catch(Exception e)
-    {
-     	e.printStackTrace(); 
-    }
-    //*/
-      fullText = "abcdefgfhhfjdksjfiioifjusydff jfhuduf";
+      }
+      catch(Exception e)
+      {
+         e.printStackTrace(); 
+      }
    }
-   public void searchFor(String goal,Map toFill)
+   public Map<Integer,String> searchFor(String goal)
    {
-         
+      Map<Integer,String> toFill = new TreeMap();
+      StringBuilder buffer = new StringBuilder(fullText);
+      int loc = buffer.indexOf(goal);
+      while(loc!=-1)
+      {
+         toFill.put(loc,buffer.substring(loc,loc+goal.length()));
+         buffer.delete(0,loc+1);
+         loc = buffer.indexOf(goal);
+      }
+      return toFill;
    }
 }
